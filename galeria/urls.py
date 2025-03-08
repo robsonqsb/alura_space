a828c38d415c28cebcadebba68a41a498e41ed4d
Robson <robsonqsb@gmail.com>
2025-03-08 12:20:23 -0300

    implementações do curso Django: cri aplicações em Python

---
+++ galeria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from galeria.views import buscar, imagem, index
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('buscar', buscar, name='buscar')
+]
